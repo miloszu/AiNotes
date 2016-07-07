afb90a11e2ce2ef1458ab8ac8884819e1bc055a3
Wierzbicki, Milosz <Milosz.Wierzbicki@btgpactual.com>
2016-07-07 17:37:30 +0100

    fix theta sampling

---
+++ chainer_test.py
@@ -0,0 +1,13 @@
+import numpy as np
+import chainer
+from chainer import cuda, Function, gradient_check, Variable, optimizers, serializers, utils
+from chainer import Link, Chain, ChainList
+import chainer.functions as F
+import chainer.links as L
+import minist_data as data
+
+data.load_mnist_data()
+
+
+
+
