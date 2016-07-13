f25a44fecbe333dc948a99ccbb0cd546eb403fb5
Wierzbicki, Milosz <Milosz.Wierzbicki@btgpactual.com>
2016-07-13 17:20:12 +0100

    nn from pybrain

--- neural_q_learning.py
+++ neural_q_learning.py
@@ -1,66 +1,80 @@
+from pybrain.supervised.trainers import BackpropTrainer
+from pybrain.tools.shortcuts import buildNetwork
+from pybrain.datasets import SupervisedDataSet
+from pybrain.tools.customxml import NetworkWriter
+from pybrain.tools.customxml import NetworkReader
+import pickle
+
 import numpy as np
 from random import randint
 import pylab as pl
-import chainer
-from chainer import cuda, Function, gradient_check, Variable, optimizers, serializers, utils
-from chainer import Link, Chain, ChainList
-import chainer.functions as F
-import chainer.links as L
-import minist_data as test_case
 import gym
-from gym.spaces import Discrete, Box
 import random
 
-class RectifierNetwork(Chain):
-    def __init__(self, in_size, out_size):
-        super(RectifierNetwork, self).__init__(
-            l1=L.Linear(in_size, 4),
-            l2=L.Linear(4, 4),
-            l3=L.Linear(4, out_size)
-        )
-
-    def __call__(self, x):
-        h1 = F.relu(self.l1(x))
-        h2 = F.relu(self.l2(h1))
-        y = self.l3(h2)
-        return y
-
-epsilon = .2
-learning_rate = .2
+save_file = True
+from_file = False
+render = False
+network_filename = "CartPole-v0.xml"
+epsilon = .6
+learning_rate = .5
+episode_steps = 200
 env = gym.make('CartPole-v0')
 
 observation_count = env.observation_space.shape[0]
 action_count = env.action_space.n - 1 #env.action_space.shape[0]
 input_count = observation_count + action_count
 discount_factor = .99
-Q = RectifierNetwork(input_count, 1)
-model = L.Classifier(Q)
-optimizer = optimizers.SGD()
-optimizer.setup(model)
+
+net = buildNetwork(input_count, input_count, 1, bias=True)
+
+if from_file:
+    fileObject = open(network_filename, 'r')
+    net = NetworkReader.readFrom(network_filename)
 
 observation = env.reset()
+counter =0
 while True:
-    action = None
-    if epsilon < random.random():
-        action_rewards = Q(np.array([np.array(np.append(observation, a)) for a in range(0,action_count + 1)], dtype=np.float32)).data
-        action = np.argmax(action_rewards)
-    else:
-        action = env.action_space.sample()
+    counter+=1
+    env.reset()
+    total_reward = 0
+    ds = SupervisedDataSet(input_count, 1)
+    #print("Q: ", end="")
+    for step in range(episode_steps):
+        action = None
+        if epsilon < random.random():
+            actions = [np.append(observation, a) for a in range(0, action_count + 1)]
+            print("Q actions: ", actions)
+            action_rewards = [net.activate(act)[0] for act in actions]
+            action = np.argmax(action_rewards)
+        else:
+            action = env.action_space.sample()
 
-    x = np.append(observation, action)
-    q_t = Q(np.array([x], dtype=np.float32)).data[0][0]
-    (observation, reward, done, _info) = env.step(action)
-    env.render()
+        x = np.append(observation, action)
+        q_t = net.activate(x)
+        (observation, reward, done, _info) = env.step(action)
+        total_reward += reward
+        if step %4 == 0 and render:
+            env.render()
 
-    q_t_max_a = max(Q(np.array([np.array(np.append(observation, a)) for a in range(0,action_count + 1)], dtype=np.float32)).data[0])
-    Q_update = q_t + learning_rate * (reward + discount_factor * q_t_max_a - q_t )
+        actions = [np.append(observation, a) for a in range(0, action_count + 1)]
+        rewards_t = [net.activate(act)[0] for act in actions]
+        q_t_max_a = max(rewards_t)
+        #print(q_t, end="")
+        y = q_t + learning_rate * (reward + discount_factor * q_t_max_a - q_t )
+        #print("Reward: {0} Q = {1} <= {2}".format(reward,q_t, y))
+        ds.addSample(x, y)
+        # if done:
+        #     break
 
-    x = Variable(np.array([x], dtype=np.float32))
-    y = Variable(np.array([np.array([Q_update])], dtype=np.float32))
+    trainer = BackpropTrainer(net, ds)
+    print("Total reward: ", total_reward, "Error: ", trainer.train(), "; Counter: ", counter)
 
-    optimizer.update(model, x, y)
-    if done:
-        env.reset()
+    if save_file and counter % 50 == 0:
+        NetworkWriter.writeToFile(net, network_filename)
+        print("Neural Network Saved to ", network_filename)
+
+
+
 
 
 
